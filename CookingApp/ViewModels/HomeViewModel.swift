1729fcf3b5ba3316c77b16310c21df359fa1e240
Giel jurriens <gieldid@gmail.com>
2026-05-19 11:20:56 +0200

    auto: HomeViewModel.swift

--- CookingApp/ViewModels/HomeViewModel.swift
+++ CookingApp/ViewModels/HomeViewModel.swift
@@ -118,7 +118,7 @@ final class HomeViewModel: ObservableObject {
 
         NotificationService.shared.scheduleAllNotifications(
             preferences: prefs.notificationPreferences,
-            recipes: allFilteredRecipes
+            recipes: [newRecipe]
         )
     }
 }
